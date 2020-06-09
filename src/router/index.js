9504070851d88a7e59863d52f6d54b2021e42247
Trần Minh Trí <tmtzminhtri@gmail.com>
2020-06-09 11:07:09 +0700

    f

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,97 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import { NotFound } from "@/views/NotFound/index.vue";
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: DashboardLayout,
+    redirect: '/admin/overview'
   },
+  // {
+  //   path: '/admin',
+  //   component: DashboardLayout,
+  //   redirect: '/admin/overview',
+  //   children: [
+  //     {
+  //       path: 'overview',
+  //       name: 'Overview',
+  //       component: Overview
+  //     },
+  //     {
+  //       path: 'new-product',
+  //       name: 'NewProduct',
+  //       component: ProductLayout,
+  //       redirect: '/product/product-category'
+  //     },
+  //     {
+  //       path: 'inbound',
+  //       name: 'Inound',
+  //       component: Inbound
+  //     },
+  //     {
+  //       path: 'tasks',
+  //       name: 'Tasks',
+  //       component: Tasks
+  //     }
+  //   ]
+  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/product',
+    component: ProductLayout,
+    redirect: '/product/product-category',
+    children: [
+      {
+        path: 'product-category',
+        name: 'Category',
+        component: ProductCategory
+      },
+      // {
+      //   path: 'product-info',
+      //   name: 'ProductInfo',
+      //   component: ProductInfo
+      // },
+      // {
+      //   path: 'product-ingredient',
+      //   name: 'ProductIngredient',
+      //   component: ProductIngredient
+      // },
+      // {
+      //   path: 'product-price',
+      //   name: 'ProductPrice',
+      //   component: ProductPrice
+      // },
+      // {
+      //   path: 'product-manufacture',
+      //   name: 'ProductManufacture',
+      //   component: ProductManufacture
+      // },
+      // {
+      //   path: 'product-supplier',
+      //   name: 'ProductSupplier',
+      //   component: ProductSupplier
+      // },
+      // {
+      //   path: 'product-description',
+      //   name: 'ProductDescription',
+      //   component: ProductDescription
+      // },
+      // {
+      //   path: 'product-package',
+      //   name: 'ProductPackage',
+      //   component: ProductPackage
+      // },
+      // {
+      //   path: 'product-importer',
+      //   name: 'ProductImporter',
+      //   component: ProductImporter
+      // }
+    ]
+  },
+  { path: '*', component: NotFound }
 ]
 
 const router = new VueRouter({
